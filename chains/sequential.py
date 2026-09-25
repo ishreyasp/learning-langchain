a528793f5d4ab6b231932f85147f341bea889f03
ishreyasp <purkarshreyas@gmail.com>
2026-09-25 18:35:34 -0400

    add sequential chain code

---
+++ chains/sequential.py
@@ -0,0 +1,24 @@
+from langchain_openai import ChatOpenAI
+from langchain_core.output_parsers import StrOutputParser
+from langchain_core.prompts import PromptTemplate
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model="gpt-4", temperature=0.4)
+
+prompt1 = PromptTemplate(
+    template='Generate a detailed report on {topic}',
+    input_variables=['topic']
+)
+
+prompt2 = PromptTemplate(
+    template='Generate a 5 pointer summary from the following text \n {text}',
+    input_variables=['text']
+)
+
+chain = prompt1 | model | StrOutputParser() | prompt2 | model | StrOutputParser()
+
+result = chain.invoke({"topic": "Artificial Intelligence"})
+
+print(result)
